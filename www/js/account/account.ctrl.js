23e9bb89b50b45d9c85a1bd4a6a6608fec372a89
Eric P. Katz <ericpkatz@gmail.com>
2016-06-10 10:41:16 -0400

    change map

--- www/js/account/account.ctrl.js
+++ www/js/account/account.ctrl.js
@@ -1,6 +1,9 @@
 angular.module('nwind')
   .controller('AccountCtrl', function($scope, Session, $window, $state, Address) {
-    $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+    $scope.setMap = function(address){
+      $scope.map = { center: { latitude: address.lat, longitude: address.lng }, zoom: 14 };
+    };
+    $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 14 };
     Session.me()
       .then(function(user){
         return Address.findAll({userId: user.id});
